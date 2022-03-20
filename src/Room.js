6285efa3848afac6c19164eba2c73d3b87c13eeb
SaroshAD <saroshahmed.07@gmail.com>
2022-03-20 22:50:40 +0500

    assignment

--- src/Room.js
+++ src/Room.js
@@ -2,17 +2,25 @@ import React, {useState} from "react";
 import './Room.css';
 const Room = () => {
     let [islit, setlit] = useState(true);
+    let [temp, setemp] = useState(32);
     return(
         <div> This Room is {islit? "lit": "Dark"} <br/>
-        <button className="flick" onClick= {() => {
-            setlit(!islit); <br/>
-            {islit? (document.body.style.color = "white") (document.body.style.backgroundColor = "black"):
-            (document.body.style.color = "black") (document.body.style.backgroundColor = "white")}
-        }}>
-        Flick the button</button>
+        <button className="on_switch" onClick= {() => {
+            setlit(true); 
+            document.body.style.color = "black"; document.body.style.backgroundColor = "white";
+        }}> Turn On </button>
+             <button className="off_switch" onClick= {() => {
+            setlit(false); 
+            document.body.style.color = "white"; document.body.style.backgroundColor = "black";
+         
+        }}> Turn Off</button> <br/> <br/>
+        The Current Temperature is {temp} C <br/>
+        <button className="increase" onClick= { () => {
+            setemp(++temp);}}> Increase</button>
+        <button className="decrease" onClick= { () => {
+            setemp(--temp);}}> Decrease</button>
         </div>
     
     )
-
 }
 export default Room;
